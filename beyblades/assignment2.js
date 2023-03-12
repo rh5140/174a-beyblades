87fa181ca5fa5db85a6301f1fedc6107b41bf96c
azcyber2009 <azcyber2009@gmail.com>
2023-03-11 16:56:53 -0800

    made draw_beyblade function; added cylinder around top of beyblade

--- beyblades/assignment2.js
+++ beyblades/assignment2.js
@@ -1,48 +1,7 @@
 import {defs, tiny} from './examples/common.js';
 
-const {Vector, Vector3, vec, vec3, vec4, color, hex_color, Matrix, Mat4, Light, Shape, Material, Scene, } = tiny;
+const {Vector, Vector3, vec, vec3, vec4, color, hex_color, Matrix, Mat4, Light, Shape, Material, Scene, Texture} = tiny;
 
-class Cube extends Shape {
-    constructor() {
-        super("position", "normal", );
-        // Loop 3 times (for each axis), and inside loop twice (for opposing cube sides):
-        this.arrays.position = Vector3.cast([-1, -1, -1], [1, -1, -1], [-1, -1, 1], [1, -1, 1], [1, 1, -1], [-1, 1, -1], [1, 1, 1], [-1, 1, 1], [-1, -1, -1], [-1, -1, 1], [-1, 1, -1], [-1, 1, 1], [1, -1, 1], [1, -1, -1], [1, 1, 1], [1, 1, -1], [-1, -1, 1], [1, -1, 1], [-1, 1, 1], [1, 1, 1], [1, -1, -1], [-1, -1, -1], [1, 1, -1], [-1, 1, -1]);
-        this.arrays.normal = Vector3.cast([0, -1, 0], [0, -1, 0], [0, -1, 0], [0, -1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0], [-1, 0, 0], [-1, 0, 0], [-1, 0, 0], [-1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, -1], [0, 0, -1], [0, 0, -1], [0, 0, -1]);
-        // Arrange the vertices into a square shape in texture space too:
-        this.indices.push(0, 1, 2, 1, 3, 2, 4, 5, 6, 5, 7, 6, 8, 9, 10, 9, 11, 10, 12, 13, 14, 13, 15, 14, 16, 17, 18, 17, 19, 18, 20, 21, 22, 21, 23, 22);
-    }
-}
-
-class Cube_Outline extends Shape {
-    constructor() {
-        super("position", "color");
-        const white = hex_color("#ffffff");
-        this.arrays.position = Vector3.cast([-1, -1, -1], [1, -1, -1], 
-                                            [1, -1, -1], [1, 1, -1], 
-                                            [1, 1, -1], [-1, 1, -1], 
-                                            [-1, 1, -1], [-1, -1, -1], 
-                                            [-1, 1, 1], [1, 1, 1], 
-                                            [1, 1, 1], [1, -1, 1], 
-                                            [1, -1, 1], [-1, -1, 1], 
-                                            [-1, -1, 1], [-1, 1, 1], 
-                                            [-1, -1, -1], [-1, -1, 1], 
-                                            [1, -1, 1], [1, -1, -1], 
-                                            [1, 1, 1], [1, 1, -1], 
-                                            [-1, 1, 1], [-1, 1, -1]);
-        this.arrays.color = [white, white, white, white, white, white, white, white, white, white, white, white, white, white, white, white, white, white, white, white, white, white, white, white];
-        this.indices = false;
-    }
-}
-
-class Cube_Single_Strip extends Shape {
-    constructor() {
-        super("position", "normal");
-        // TODO (Requirement 6)
-        this.arrays.position = Vector3.cast([1, 1, -1], [-1, 1, -1], [1, 1, 1], [-1, 1, 1], [1, -1, -1], [-1, -1, -1], [-1, -1, 1], [1, -1, 1]);
-        this.arrays.normal = Vector3.cast([1, 1, -1], [-1, 1, -1], [1, 1, 1], [-1, 1, 1], [1, -1, -1], [-1, -1, -1], [-1, -1, 1], [1, -1, 1]);
-        this.indices.push(0, 1, 4, 5, 6, 1, 3, 0, 2, 4, 7, 6, 2, 3);
-    }
-}
 
 class Base_Scene extends Scene {
     /**
